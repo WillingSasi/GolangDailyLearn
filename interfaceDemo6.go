b1c68a23672e16825a4819bbb70e077e351fb4ca
WillingSasi <15522578697@163.com>
2022-01-06 23:09:35 +0800

    x

--- interfaceDemo6.go
+++ interfaceDemo6.go
@@ -1,7 +1,42 @@
-package sort
+package main
 
-type Interface interface {
-	Len() int
-	Less(i, j int) bool
-	Swap(i, j int)
+import (
+	"fmt"
+	"sort"
+)
+
+// type Interface interface {
+// 	Len() int
+// 	Less(i, j int) bool
+// 	Swap(i, j int)
+// }
+
+type MyStringList []string
+
+func (m MyStringList) Len() int {
+	return len(m)
+}
+
+func (m MyStringList) Less(i, j int) bool {
+	return m[i] < m[j]
+}
+
+func (m MyStringList) Swap(i, j int) {
+	m[i], m[j] = m[j], m[i]
+}
+
+func main() {
+	name := MyStringList{
+		"3. Triple Kill",
+		"5. Penta Kill",
+		"2. Double Kill",
+		"4. Quadra Kill",
+		"1. First Blood",
+	}
+
+	sort.Sort(name)
+
+	for _, v := range name {
+		fmt.Printf("%s\n", v)
+	}
 }
